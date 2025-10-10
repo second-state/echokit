151cb7e35e02909731b676b0618e52dc3f8ffa8a
alabulei1 <vivian.xiage@gmail.com>
2025-10-10 18:51:33 +0800

    Update docusaurus.config.js

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/echokit-b-sample.png',
       navbar: {
         title: 'EchoKit',
         logo: {
