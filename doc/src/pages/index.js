f4236a57ed3ae28044bd5860e1af6cfd9e6f5284
Michael Yuan <michael@secondstate.io>
2025-11-23 14:19:41 -0500

    fix links
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/get-started">
+            to="/docs/get-started">
             Quick start for your device
           </Link>
         </div>
