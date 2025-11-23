d1a7dd45177432bec8af96c8fd0bb92e5487fac8
Michael Yuan <michael@michaelyuan.com>
2025-11-23 14:36:15 -0500

    Update index.js

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/get-started">
+            to="/docs/get-started/echokit-diy">
             Quick start for your device
           </Link>
         </div>
